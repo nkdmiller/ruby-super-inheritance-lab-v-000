486a435ebd21fcb5f6fabc9e6dd25e9f4aa6beb8
Nate Miller <natekmiller96@gmail.com>
2018-07-05 17:31:43 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,7 +3,7 @@ class ChattyStudent < Student
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
-  def pick_me
+  def raise_hand
     counter = 0
     loop do
       counter += 1
